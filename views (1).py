282a905f6dbac4f63a1ede8bd34e7476105d7292
ajeetram <86039147+ajeetram@users.noreply.github.com>
2021-06-21 13:13:34 +0530

    Add files via upload

---
+++ views (1).py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .form import ImageForm
+from .models import Image
+
+
+def index(request):
+    if request.method == "POST":
+        form=ImageForm(data=request.POST,files=request.FILES)
+        if form.is_valid():
+            form.save()
+            obj=form.instance
+            return render(request,"index.html",{"obj":obj})
+    else:
+        form=ImageForm()
+        img=Image.objects.all()
+    return render(request,"index.html",{"img":img,"form":form})
